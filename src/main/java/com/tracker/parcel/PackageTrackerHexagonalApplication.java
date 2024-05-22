bea81ab1e06e745e09ee41ce167047a43275f108
Bohdan Siabruk <bogdan.siabruk@nixsolutions.com>
2024-05-22 21:13:03 +0200

    Initial commit

---
+++ src/main/java/com/tracker/parcel/PackageTrackerHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.parcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageTrackerHexagonalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PackageTrackerHexagonalApplication.class, args);
+  }
+
+}
